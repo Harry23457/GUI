0875a827d308efa624ba2d9b42a19abcb8c51bba
Harry23457 <southwelle@hotmail.com>
2025-07-20 08:54:54 +0100

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,35 @@
+from tkinter import *
+from tkinter.ttk import *
+root=Tk()
+root.title("website")
+#creating a menu bar widget
+menubar=Menu(root)
+file=Menu(menubar,tearoff=0)
+menubar.add_cascade(label="File",menu=file)
+file.add_command(label="New File",command=None)
+file.add_separator()
+file.add_command(label="Open Folder",command=None)
+file.add_separator()
+file.add_command(label="Save As",command=None)
+file.add_separator()
+file.add_command(label="Share",command=None)
+file.add_separator()
+file.add_command(label="Exit",command=None)
+file.add_separator()
+
+edit=Menu(menubar,tearoff=0)
+menubar.add_cascade(label="Edit",menu=edit)
+edit.add_command(label="Undo",command=None)
+edit.add_separator()
+edit.add_command(label="Redo",command=None)
+edit.add_separator()
+edit.add_command(label="Copy",command=None)
+edit.add_separator()
+edit.add_command(label="Paste",command=None)
+edit.add_separator()
+edit.add_command(label="Cut",command=None)
+edit.add_separator()
+
+
+root.config(menu=menubar)
+mainloop()
