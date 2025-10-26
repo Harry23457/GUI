83c02b61fbd64504bb516cf9f1567046a182432c
Harry23457 <southwelle@hotmail.com>
2025-10-26 09:57:56 +0000

    Add files via upload

--- multiplication.py
+++ multiplication.py
@@ -3,6 +3,18 @@ from tkinter.ttk import *
 window=Tk()
 #indow.geometry("200x200")
 
+def result():
+    n1=number.get()
+    n2=multiples.get()
+    finalanwser=""
+    for i in range(1,n2+1):
+        anwser=str(n1)+"x"+str(i)+"="+str(n1*i)+"\n"
+
+        finalanwser=finalanwser+anwser
+
+    l.config(text=finalanwser)
+
+
 heading1=Label(window,text="Multiplication table")
 heading1.grid(row=0,column=0,columnspan=2,pady=20,padx=20)
 heading2=Label(window,text="Number")
@@ -22,9 +34,15 @@ r20.grid(row=3,column=1)
 r30=Radiobutton(window,text=30,value=30,variable=multiples)
 r30.grid(row=4,column=1)
 multiples.set(10)
-g=Button(window,text="Generate")
+g=Button(window,text="Generate",command=result)
 g.grid(row=5,column=0,columnspan=2,pady=20,padx=20)
 l=Label(window)
 l.grid(row=6,column=0,columnspan=2,pady=10,padx=10)
+
+
+
+
+
+
 
 window.mainloop()
