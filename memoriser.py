a824a0b8a2406f1b8a4bc1ffed5a2318cc330dc6
Harry23457 <southwelle@hotmail.com>
2025-11-23 09:59:54 +0000

    Add files via upload

--- memoriser.py
+++ memoriser.py
@@ -1,25 +1,56 @@
 from tkinter import *
+from tkinter.filedialog import *
 
+def additem():
+    item=e.get()
+    l.insert(END,item)
+
+def deleteitem():
+    index=l.curselection()
+    print(index)
+    if index :
+        index=index[::-1]
+        for i in index:
+            l.delete (i)
+
+
+def savelist():
+    f=asksaveasfile()
+    if f :
+        for item in l.get(0,END):
+            print (item,file=f)
+        l.delete(0,END)
+
+def openlist():
+    p=askopenfile()
+    if p :
+        l.delete(0,END)
+        items=p.readlines()
+        for item in items:
+            l.insert(END,item.strip())                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+            
 window=Tk()
 window.geometry("400x500")
 
-a=Button(window,text="Add")
+a=Button(window,text="Add",command=additem)
 a.pack()
+
 
 e=Entry(window)
 e.pack(pady=10)
 
-d=Button(window,text="Delete")
+d=Button(window,text="Delete",command=deleteitem)
 d.pack(pady=10)
 
-l=Listbox(window)
+l=Listbox(window,selectmode="multiple")
 l.pack()
 
-s=Button(window,text="save")
+s=Button(window,text="save",command=savelist)
 s.pack()
 
-o=Button(window,text="open")
+o=Button(window,text="open",command=openlist)
 o.pack()
+
 
 
 
