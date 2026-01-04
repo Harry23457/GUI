34c4a5a0b144a8d026533977c71aa1243daacb65
Harry23457 <southwelle@hotmail.com>
2026-01-04 09:54:19 +0000

    Add files via upload

--- random.py
+++ random.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import Tk
 e=Tk()
-e.geometry("150x150")
+e.geometry("300x300")
 e.config(bg="red")
 
 l1=Label(e,text="Pizza ordering website")
@@ -12,12 +12,29 @@ l1.grid(row=0,column=1)
 pizza1=StringVar()
 pizza2=["Pepperoni","magerita","Ham and pinapple","meat feast","Salad"]
 box=ttk.Combobox(e,textvariable=pizza1,values=pizza2)
-box.grid(row=2,column=1)
+box.grid(row=1,column=1)
+
+pizza3=StringVar()
+pizza4=["Lemonade","Cola","Fanta","Water","Nothing"]
+box2=ttk.Combobox(e,textvariable=pizza3,values=pizza4)
+box2.grid(row=2 ,column=1)
+
+pizza5=StringVar()
+pizza6=["Small","Medium","Large","Extra large"]
+box3=ttk.Combobox(e,textvariable=pizza5,values=pizza6)
+box3.grid(row=3,column=1)
 
 
-pizza3=["1","2","3","4","5","6","7","8","9","10"]
-box2=ttk.combobox(e,textvariable=pizza1,values=pizza3)
-box2.grid(row=2,column=1)
+def ordered():
+    pizza=pizza1.get()
+    drink=pizza3.get()
+    size=pizza5.get()
+    l1=Label(e,text=f"You ordered a {size} {pizza} and a {drink} drink")
+    l1.grid(row=5,column=1)
+
+
+b1=Button(e,text="place order",command=ordered)
+b1.grid(row=4,column=1)
 
 
 
